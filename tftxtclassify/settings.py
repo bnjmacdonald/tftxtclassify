abd33f84d105ca3cdba9dfa5b772f4060f3750d3
Bobbie Macdonald <bnjmacdonald@gmail.com>
2018-03-05 01:04:54 -0800

    rename constants in settings.py

--- tftxtclassify/settings.py
+++ tftxtclassify/settings.py
@@ -1,8 +1,10 @@
 """project settings."""
 import os
-CODE_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_DIR = os.path.abspath(os.path.join(CODE_DIR, '..'))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
 DATA_DIR = os.path.join(PROJECT_DIR, 'data')
 OUTPUT_DIR = os.path.join(PROJECT_DIR, 'output')
+TEST_DIR = os.path.join(os.path.dirname(BASE_DIR), 'test')
+
 if __name__ == '__main__':
     print(PROJECT_DIR)
