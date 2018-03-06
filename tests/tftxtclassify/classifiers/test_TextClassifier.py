578e662472e055924efda99587c34bc834fb1a06
Bobbie Macdonald <bnjmacdonald@gmail.com>
2018-03-05 23:45:07 -0800

    add simple tests for use of birectional rnn, pretrained_embeddings, and online sample

--- tests/tftxtclassify/classifiers/test_TextClassifier.py
+++ tests/tftxtclassify/classifiers/test_TextClassifier.py
@@ -35,14 +35,16 @@ class TextClassifierTests(unittest.TestCase):
         # constructs mock data
         n = 50  # number of observations
         pad = 0  # sequence padding
-        max_seqlen = 50  # maximum sequence length
-        self.inputs = np.random.randint(0, self.vocab_size, size=(n, max_seqlen))
-        self.seqlens = np.random.randint(1, max_seqlen, size=n)
+        n_features = 50  # maximum sequence length
+        self.inputs = np.random.randint(0, self.vocab_size, size=(n, n_features))
+        self.seqlens = np.random.randint(1, n_features, size=n)
         for i in range(self.inputs.shape[0]):
             self.inputs[i,self.seqlens[i]:] = pad
         self.labels = np.random.binomial(1, p=0.2, size=n)  # binary labels
         n_classes = 10
         self.multi_labels = np.random.randint(0, n_classes, size=n)  # multi-class labels (in sparse format)
+        self.embed_size = 25
+        self.pretrained_embeddings = np.random.random(size=(self.vocab_size, self.embed_size))
 
 
     def tearDown(self):
@@ -85,7 +87,6 @@ class TextClassifierTests(unittest.TestCase):
         with minimal configuration.
         """
         for classifier in TextClassifier.ACCEPTABLE_CLASSIFIERS:
-            print(f'memorizing data for classifier {classifier}...')
             try:
                 tf.reset_default_graph()
                 with tf.Session() as sess:
@@ -217,6 +218,150 @@ class TextClassifierTests(unittest.TestCase):
                     # test 2: `perf_train` should have an accuracy that is close to 1.
                     self.assertGreater(perf['accuracy'], 0.95)
                     print('FYI - Performance on train set: ', perf)
+                    writer.close()
+            except Exception as err:
+                print(err)
+                traceback.print_exc()
+                self.fail('self.build_graph() raised an error.')
+
+
+    def test_pretrained_embeddings(self):
+        """tests that `train.main` method executes without error when using
+        pretrained embeddings.
+
+        Todos:
+
+            TODO: test that embeddings are not trainable.
+
+            TODO: test that pretrained embeddings are actually assigned to `self._token_embeds`.
+        """
+        embed_trainable = True
+        for classifier in TextClassifier.ACCEPTABLE_CLASSIFIERS:
+            try:
+                tf.reset_default_graph()
+                with tf.Session() as sess:
+                    config = get_config_class(classifier)(
+                        classifier=classifier,
+                        outpath=self.outpath,
+                        n_examples=self.inputs.shape[0],
+                        n_features=self.inputs.shape[1],
+                        n_classes=np.unique(self.labels).shape[0],
+                        n_epochs=2,
+                        batch_size=32,
+                        vocab_size=self.vocabulary.shape[0],
+                        use_pretrained=True,
+                        embed_size=self.embed_size
+                    )
+                    clf = TextClassifier(
+                        sess=sess,
+                        config=config,
+                        vocabulary=self.vocabulary,
+                        verbosity=self.verbosity
+                    )
+                    clf.build_graph()
+                    writer = tf.summary.FileWriter(self.outpath, sess.graph)
+                    clf.train(
+                        inputs=self.inputs,
+                        seqlens=self.seqlens,
+                        labels=self.labels,
+                        writer=writer,
+                        pretrained_embeddings=self.pretrained_embeddings
+                    )
+                    writer.close()
+                embed_trainable = not embed_trainable  # runs tests with both embed_trainable == True and False.
+            except Exception as err:
+                print(err)
+                traceback.print_exc()
+                self.fail('self.build_graph() raised an error.')
+
+
+    def test_rnn_bidirectional(self):
+        """tests that `train.main` method executes without error when using
+        birectional RNN.
+
+        Todos:
+
+            TODO: add test that verifies bi-directional Ops actually exist in the
+                graph.
+        """
+        classifier = 'rnn'
+        try:
+            tf.reset_default_graph()
+            with tf.Session() as sess:
+                config = get_config_class(classifier)(
+                    classifier=classifier,
+                    outpath=self.outpath,
+                    n_examples=self.inputs.shape[0],
+                    n_features=self.inputs.shape[1],
+                    n_classes=np.unique(self.labels).shape[0],
+                    n_epochs=2,
+                    batch_size=32,
+                    vocab_size=self.vocabulary.shape[0],
+                    use_bidirectional=True,
+                )
+                clf = TextClassifier(
+                    sess=sess,
+                    config=config,
+                    vocabulary=self.vocabulary,
+                    verbosity=self.verbosity
+                )
+                clf.build_graph()
+                writer = tf.summary.FileWriter(self.outpath, sess.graph)
+                clf.train(
+                    inputs=self.inputs,
+                    seqlens=self.seqlens,
+                    labels=self.labels,
+                    writer=writer
+                )
+                writer.close()
+        except Exception as err:
+            print(err)
+            traceback.print_exc()
+            self.fail('self.build_graph() raised an error.')
+
+
+    def test_online_sampling(self):
+        """tests that `train.main` method executes without error when using
+        online sample args.
+
+        Todos:
+
+            TODO: add tests that verifies online sampling actually happens.
+
+        """
+        classifier = 'cnn'
+        for how in ['soft', 'hard', 'hard_positives', 'hard_negatives']:
+            try:
+                tf.reset_default_graph()
+                with tf.Session() as sess:
+                    config = get_config_class(classifier)(
+                        classifier=classifier,
+                        outpath=self.outpath,
+                        n_examples=self.inputs.shape[0],
+                        n_features=self.inputs.shape[1],
+                        n_classes=np.unique(self.labels).shape[0],
+                        n_epochs=2,
+                        batch_size=32,
+                        vocab_size=self.vocabulary.shape[0],
+                        online_sample=how,
+                        online_sample_n_keep=10,
+                        online_sample_after=0,
+                        online_sample_every=1,
+                    )
+                    clf = TextClassifier(
+                        sess=sess,
+                        config=config,
+                        vocabulary=self.vocabulary,
+                        verbosity=self.verbosity
+                    )
+                    clf.build_graph()
+                    writer = tf.summary.FileWriter(self.outpath, sess.graph)
+                    clf.train(
+                        inputs=self.inputs,
+                        seqlens=self.seqlens,
+                        labels=self.labels,
+                        writer=writer
+                    )
                     writer.close()
             except Exception as err:
                 print(err)
